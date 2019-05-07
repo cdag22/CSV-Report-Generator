fb6f045fd580af873be368dac822fe1535bee26f
cj dagostino <cjd.dagostino@gmail.com>
2019-05-07 16:41:54 -0600

    users can upload, convert, and download JSON->CSV files.

--- server.js
+++ server.js
@@ -5,17 +5,38 @@ const util = require('./utilities');
 
 const app = express();
 const port = 3000;
+const csvFilePath = './csv-data.csv';
+
 let csv = '';
 
 // app.use(bodyParser.raw());
+app.use(express.static(__dirname + '/public'));
 app.use(express.static('views'));
 app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
   res.status(200);
   res.render('index', { csv });
 });
 
+app.get('/download_csv', (req, res) => {
+  if (csv) {
+    fs.writeFile(csvFilePath, csv, { encoding: 'utf8' }, (error) => {
+      if (error) {
+        res.status(500);
+        res.redirect('/');
+      } else {
+        res.setHeader('Content-disposition', 'attachment; filename=csv-data.csv');
+        res.set('Content-Type', 'text/csv');
+        res.status(200).send(csv);
+      }
+    });
+  } else {
+    res.status(404);
+    res.redirect('/');
+  }
+});
 
 app.post('/upload_csv', (req, res) => {
   let buff = [];
@@ -25,12 +46,11 @@ app.post('/upload_csv', (req, res) => {
   req.on('end', () => {
     let data = Buffer.concat(buff).toString();
     csv = util.convertToCSV(data);
-    // console.log('csv', csv);
     res.status(200);
     res.redirect('/');
   });
-
 });
+
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`)
