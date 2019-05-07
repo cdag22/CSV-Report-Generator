6d26d37a8d470cf873a37e783bdb1a23a30ab765
cj dagostino <cjd.dagostino@gmail.com>
2019-05-07 14:30:30 -0600

    CSV rendered on page with ejs template.

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const util = require('./utilities');
 
-const template = require('./responseTemplate');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.text());
 app.use(express.static('client'));
-
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.status(200);
@@ -18,8 +17,7 @@ app.get('/', (req, res) => {
 app.post('/upload_csv', (req, res) => {
   let csv = util.convertToCSV(req.body);
   res.status(200);
-  res.render(template, { csv })
-  res.redirect('/');
+  res.render('postResponse', { csv });
 });
 
 app.listen(port, () => {
